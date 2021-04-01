83060b0c52931e504c3d3caf365a8a53d24491da
Pieli <you@example.com>
2021-04-01 11:15:48 +0200

    Implemented Game like state

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Game.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Game.java
@@ -1,27 +1,69 @@
 package ch.uzh.ifi.hase.soprafs21.entity;
 
-import java.util.Map;
-import java.util.Queue;
+import org.hibernate.mapping.Map;
 
-public class Game {
+import ch.uzh.ifi.hase.soprafs21.entity.patterns.Observer;
+import ch.uzh.ifi.hase.soprafs21.entity.patterns.SubjetObserver;
+
+public class Game implements SubjetObserver{
 
     public Game(){
         // Assign 5 random questions to Question[]
+    private final String gameId;
+    private final String roomKey;
+    private GameStatus status;
+    private Question[] questions;
+    private final UserMode userMode;
+    private final GameMode gameMode;
+
+    // brauchen wir das?
+    private Map<String, Integer> userScores;
     }
-    public GameMode gameMode;
-    public UserMode userMode;
-    public int Id;
-    public int roomKey;
-    public GameStatus status;
-    public Map<String, Integer> userScores;
-    public Question[] questions;
 
+    @Override
+    public void registerObserver(Observer observer) {
+        // TODO Auto-generated method stub
+        
+    }
 
+    @Override
+    public void removeObserver(Observer observer) {
+        // TODO Auto-generated method stub
 
-    public void calculateUserScore(float userX, float userY, int questionId){
+    }
+
+    @Override
+    public void notifyObserver() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void startGame(String gameId, String userId){
+        throw new UnsupportedOperationException();
+        
+    }
+
+    public void startGame(String gameId, String userId){
+    }
+
+    public Question nextQuestion(String gameId){
+    }
+
+    public void exitGame(String userId, String gameId){
+
+    }
+
+    public Score calculateUserScore(int gameDuration, int difficulty, GameMode gameMode, String LatLang, String questionID, String UserID){
         //this.gameMode.calculateScore()
+        // notifyOverver()
+        return new Score();
     }
+    
+    
+    
 
+
+    
 
 }
 
