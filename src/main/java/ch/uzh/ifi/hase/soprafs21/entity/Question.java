09453613d6ee161726bd3aad826aa438634d093b
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-04-06 11:41:40 +0200

    Created Score and Question entity.

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Question.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Question.java
@@ -1,13 +1,56 @@
 package ch.uzh.ifi.hase.soprafs21.entity;
+import javax.persistence.*;
 
+
+@Entity
+@Table(name = "Question")
 public class Question {
 
-    public int questionId;
+
+
+    @Column(nullable = false)
     public float zoomLevel;
+    @Column(nullable = false)
     public float lat;
+    @Column(nullable = false)
     public float lng;
+    @Id
+    @GeneratedValue
+    private Long questionId;
 
 
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
+
+    @Id
+    public Long getQuestionId() {
+        return questionId;
+    }
+
+    public float getZoomLevel() {
+        return zoomLevel;
+    }
+
+    public void setZoomLevel(float zoomLevel) {
+        this.zoomLevel = zoomLevel;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
 }
 
 
--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Score.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Score.java
@@ -1,8 +1,60 @@
 package ch.uzh.ifi.hase.soprafs21.entity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+
+@Entity
+@Table(name = "Score")
 public class Score {
-    public String userId;
+    @Id
+    @Column(nullable = false, unique = true)
+    public long userId;
+
+    @Column(nullable = false, unique = true)
     public String userName;
+
+    @Column(nullable = false)
     public int gameModeId;
+
+    @Column(nullable = false)
     public int score;
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getGameModeId() {
+        return gameModeId;
+    }
+
+    public void setGameModeId(int gameModeId) {
+        this.gameModeId = gameModeId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+
+    public long getUserId() {
+        return userId;
+    }
 }
