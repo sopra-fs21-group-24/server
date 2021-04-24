482b02d7f53c54e16deca1cb682ece091e1e9a47
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-04-24 21:13:54 +0200

    Updated Question #43 #24

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Question.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/Question.java
@@ -15,33 +15,34 @@ public class Question implements Serializable{
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue
+    //@GeneratedValue    for testing
     private Long questionId;
 
     @Column(nullable = false)
-    public float zoomLevel;
+
+    private int zoomLevel;
 
     @Column(nullable = false)
-    public float lat;
+    private float lat;
 
     @Column(nullable = false)
-    public float lng;
+    private float lng;
+
 
     @Column(nullable = false)
-    public Coordinate coordinate;
+    private Coordinate coordinate;
 
 
-    @Id
     public Long getQuestionId() {
         return questionId;
     }
 
-    public float getZoomLevel() {
+    public int getZoomLevel() {
         return zoomLevel;
     }
 
-    public void setZoomLevel(float zoomLevel) {
-        this.zoomLevel = zoomLevel;
+    public void setZoomLevel(int zoomLevel) {
+        this.zoomLevel =  zoomLevel;
     }
 
     public Coordinate getCoordinate() {
@@ -50,6 +51,26 @@ public class Question implements Serializable{
 
     public void setCoordinate(Coordinate coordinate) {
         this.coordinate = coordinate;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
+
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
     }
 }
 
