3781984f97faf40cda06468e06f8d176a1f352b9
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-04-24 21:15:06 +0200

    Implemented get Qustion to fetch Image for FE #43

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/rest/dto/QuestionGetDTO.java
@@ -0,0 +1,26 @@
+package ch.uzh.ifi.hase.soprafs21.rest.dto;
+
+public class QuestionGetDTO {
+
+    private Integer height;
+    private Integer width;
+
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWidth() {
+        return width;
+    }
+
+    public void setWidth(Integer width) {
+        this.width = width;
+    }
+
+
+}
