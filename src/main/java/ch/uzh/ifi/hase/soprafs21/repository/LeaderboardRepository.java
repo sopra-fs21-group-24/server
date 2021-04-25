a2a3dff4af3d74563817fa7d6cad952c10bd38b3
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-04-25 16:26:44 +0200

    Implemented Leaderboard to get top 5 Leaderboards of gameMode close close #37

--- src/main/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepository.java
@@ -1,12 +1,16 @@
 package ch.uzh.ifi.hase.soprafs21.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import ch.uzh.ifi.hase.soprafs21.entity.Leaderboard;
 import ch.uzh.ifi.hase.soprafs21.entity.gameModeEnum;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 @Repository("leaderboardRepository")
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Long> {
-    Leaderboard findBygameMode(gameModeEnum gameMode);
+    List<Leaderboard> findByTop5GameMode(gameModeEnum gameMode);
 }
