0890004191c1e3a2c66d9078c741d4c4f52aa07d
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-05-05 09:57:59 +0200

    Changed API key to Heroku loval variable

--- src/main/java/ch/uzh/ifi/hase/soprafs21/service/QuestionService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/service/QuestionService.java
@@ -12,6 +12,7 @@ import ch.uzh.ifi.hase.soprafs21.exceptions.PreconditionFailedException;
 import org.aspectj.util.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,9 +41,9 @@ public class QuestionService {
 
     public String getMapImage(int height, int width, Question question) throws MalformedURLException {
         try {
-            String apikey = "AIzaSyCbLudPiesxon89uVFg9qloApgl_8BXviY";
+
             String url = "https://maps.googleapis.com/maps/api/staticmap?center="+question.getCoordinate().getLat()+","+question.getCoordinate().getLon()+"&zoom="+ question.getZoomLevel() +
-                    "&size="+ height +"x"+ width +"&scale=2&maptype=satellite&key="+apikey;
+                    "&size="+ height +"x"+ width +"&scale=2&maptype=satellite&key="+System.getenv("apikey");
 
             URL mapUrl = new URL(url);
             HttpURLConnection httpURLConnection = (HttpURLConnection) mapUrl.openConnection();
