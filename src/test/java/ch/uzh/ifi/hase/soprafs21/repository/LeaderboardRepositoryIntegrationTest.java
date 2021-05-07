58750ec0205007f627803cd526785c6dc3f0aee8
Pieli <you@example.com>
2021-05-07 15:18:14 +0200

    Fixed LeaderboardRepoIntegrationTest

--- src/test/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepositoryIntegrationTest.java
@@ -1,9 +1,15 @@
 package ch.uzh.ifi.hase.soprafs21.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import ch.uzh.ifi.hase.soprafs21.entity.Leaderboard;
 
 @DataJpaTest
 public class LeaderboardRepositoryIntegrationTest {
@@ -17,43 +23,43 @@ public class LeaderboardRepositoryIntegrationTest {
     @Test
     public void findAll_success() {
         // given
-       /*  Leaderboard leaderboard = new Leaderboard();
-        leaderboard.setId(1L);
+        Leaderboard leaderboard = new Leaderboard();
+        //leaderboard.setId(1L);
         leaderboard.setGameMode("Clouds");
         leaderboard.setScore(1000L);
         leaderboard.setUsername("user1");
         entityManager.persistAndFlush(leaderboard);
 
         Leaderboard leaderboard2 = new Leaderboard();
-        leaderboard2.setId(2L);
+        // leaderboard2.setId(2L);
         leaderboard2.setGameMode("Clouds");
         leaderboard2.setScore(1001L);
         leaderboard2.setUsername("user2");
         entityManager.persistAndFlush(leaderboard2);
 
         Leaderboard leaderboard3 = new Leaderboard();
-        leaderboard3.setId(3L);
+        // leaderboard3.setId(3L);
         leaderboard3.setGameMode("Clouds");
         leaderboard3.setScore(1001L);
         leaderboard3.setUsername("user3");
         entityManager.persistAndFlush(leaderboard3);
 
         Leaderboard leaderboard4 = new Leaderboard();
-        leaderboard4.setId(4L);
+        // leaderboard4.setId(4L);
         leaderboard4.setGameMode("Clouds");
         leaderboard4.setScore(1002L);
         leaderboard4.setUsername("user4");
         entityManager.persistAndFlush(leaderboard4);
 
         Leaderboard leaderboard5 = new Leaderboard();
-        leaderboard5.setId(5L);
+        // leaderboard5.setId(5L);
         leaderboard5.setGameMode("Clouds");
         leaderboard5.setScore(1003L);
         leaderboard5.setUsername("user5");
         entityManager.persistAndFlush(leaderboard5);
 
         Leaderboard leaderboard6 = new Leaderboard();
-        leaderboard6.setId(6L);
+        // leaderboard6.setId(6L);
         leaderboard6.setGameMode("Time");
         leaderboard6.setScore(1004L);
         leaderboard6.setUsername("user6");
@@ -64,7 +70,7 @@ public class LeaderboardRepositoryIntegrationTest {
         // then
         assertEquals(found.size(),5);
         found.forEach(l->{
-            assertEquals(true, l.getGameMode() == "Clouds");
-        });*/
+            assertEquals("Clouds", l.getGameMode());
+        });
     } 
 }
