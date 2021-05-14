9ca8a7f7f4653e29323e412a63fe4605a0a6dcd7
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-05-14 11:17:05 +0200

    Createt SQL Query for Leaderboard repo - closes #84

--- src/main/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepository.java
@@ -1,6 +1,7 @@
 package ch.uzh.ifi.hase.soprafs21.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ch.uzh.ifi.hase.soprafs21.entity.Leaderboard;
 
@@ -9,5 +10,6 @@ import java.util.List;
 
 @Repository("leaderboardRepository")
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, String> {
+    @Query("select l from Leaderboard l where l.gameMode = ?1 and l.score in(select max(n.score) from Leaderboard n where l.username = n.username) order by l.score desc")
     List<Leaderboard> findTop5ByGameModeOrderByScoreDesc(String gameMode);
 }
