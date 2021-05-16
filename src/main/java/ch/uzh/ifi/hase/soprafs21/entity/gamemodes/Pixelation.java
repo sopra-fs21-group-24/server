81b0d19bb8999fd18e32a8eb1b1aa6e5260304dd
Pieli <papaya@jupiter.com>
2021-05-16 11:22:50 +0200

    Removed difficulty factor in Pixelation mode

--- src/main/java/ch/uzh/ifi/hase/soprafs21/entity/gamemodes/Pixelation.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/entity/gamemodes/Pixelation.java
@@ -2,11 +2,19 @@ package ch.uzh.ifi.hase.soprafs21.entity.gamemodes;
 
 import java.io.Serial;
 
+import ch.uzh.ifi.hase.soprafs21.entity.Answer;
+
 public class Pixelation extends GameMode {
     @Serial
     private static final long serialVersionUID = 1L;
 
     private String name = "Pixelation";
+
+    @Override
+    public Long calculateScore(Answer answer) {
+        answer.setDifficultyFactor(1);
+        return super.calculateScore(answer);
+    }
 
     public String getName() {
         return name;
