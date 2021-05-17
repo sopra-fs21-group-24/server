c7823356f9e510d0400f48ebc5a8536b530c8b21
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-05-17 08:29:31 +0200

    Debuged Leaderboard query

--- src/main/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs21/repository/LeaderboardRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 @Repository("leaderboardRepository")
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, String> {
-    @Query("select l from Leaderboard l where l.gameMode = ?1 and l.score in(select max(n.score) from Leaderboard n where l.username = n.username) order by l.score desc")
+    @Query("select l from Leaderboard l where l.gameMode = ?1 and l.score in(select max(n.score) from Leaderboard n where l.username = n.username and n.gameMode = ?1) order by l.score desc")
     List<Leaderboard> findTop5ByGameModeOrderByScoreDesc(String gameMode);
 }
