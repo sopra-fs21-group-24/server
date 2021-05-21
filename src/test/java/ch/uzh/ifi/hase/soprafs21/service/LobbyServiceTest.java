4b887507f909e04fc06175c7993afbc8cb58ee7e
Dave5252 <71384900+Dave5252@users.noreply.github.com>
2021-05-21 14:51:40 +0200

    Added new testcases in LobbyServiceTest

--- src/test/java/ch/uzh/ifi/hase/soprafs21/service/LobbyServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs21/service/LobbyServiceTest.java
@@ -1,14 +1,15 @@
 package ch.uzh.ifi.hase.soprafs21.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.mockito.BDDMockito.given;
 
 import java.util.Optional;
 
+import ch.uzh.ifi.hase.soprafs21.entity.GameEntity;
 import ch.uzh.ifi.hase.soprafs21.exceptions.NotFoundException;
 import ch.uzh.ifi.hase.soprafs21.exceptions.PreconditionFailedException;
+import ch.uzh.ifi.hase.soprafs21.repository.GameRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -21,6 +22,7 @@ import ch.uzh.ifi.hase.soprafs21.entity.User;
 import ch.uzh.ifi.hase.soprafs21.repository.LobbyRepository;
 import ch.uzh.ifi.hase.soprafs21.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.util.Assert;
 
 public class LobbyServiceTest {
 
@@ -31,6 +33,11 @@ public class LobbyServiceTest {
     @Mock
     @Qualifier("userRepository")
     private UserRepository userRepository;
+
+
+    @Mock
+    @Qualifier("gameRepository")
+    private GameRepository gameRepository;
 
     @InjectMocks
     private LobbyService lobbyService;
@@ -63,12 +70,17 @@ public class LobbyServiceTest {
         testlobby.setId(4L);
         testlobby.setPublicStatus(true);
         testlobby.setGameId(3L);
+        GameEntity game = new GameEntity();
+
+
 
         // when -> any object is being save in the userRepository -> return the dummy testUser
-        Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser);
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser, testUser2);
         Mockito.when(lobbyRepository.saveAndFlush(Mockito.any())).thenReturn(testlobby);
         Mockito.when(lobbyRepository.findById(Mockito.any())).thenReturn(Optional.ofNullable(testlobby));
-        Mockito.when(userRepository.findById(Mockito.any())).thenReturn(Optional.ofNullable(testUser));
+        Mockito.when(userRepository.findById(testUser.getId())).thenReturn(Optional.ofNullable(testUser));
+        Mockito.when(userRepository.findById(testUser2.getId())).thenReturn(Optional.ofNullable(testUser2));
+        Mockito.when(gameRepository.findByLobbyId(Mockito.anyLong())).thenReturn(Optional.of(game));
     }
 
 
@@ -130,10 +142,10 @@ public class LobbyServiceTest {
     @Test
     public void adduserToExistingLobby() {
 
-/*
         userService.createUser(testUser);
         userService.createUser(testUser2);
-
+        GameEntity game = new GameEntity();
+        Mockito.when(gameRepository.findByLobbyId(Mockito.anyLong())).thenReturn(Optional.of(game));
 
         Lobby createdLobby = lobbyService.createLobby(testlobby);
         lobbyService.addUserToExistingLobby(testUser2, createdLobby);
@@ -149,7 +161,29 @@ public class LobbyServiceTest {
         assertEquals(true, testUser.getInLobby());
         assertEquals(true, testUser2.getInLobby());
         assertEquals(testlobby.getPublicStatus(), createdLobby.getPublicStatus());
- */
+
+    }
+
+    @Test
+    public void adduserToExistingLobbyFail() {
+
+
+        Lobby testlobby2 = new Lobby();
+        testlobby2.setCreator(testUser2.getId());
+        testlobby2.setId(7L);
+        testlobby2.setPublicStatus(true);
+        testlobby2.setGameId(8L);
+        testUser2.setInLobby(false);
+        userService.createUser(testUser);
+        userService.createUser(testUser2);
+
+
+        Lobby createdLobby = lobbyService.createLobby(testlobby);
+        Mockito.when(lobbyService.createLobby(testlobby2)).thenReturn(testlobby2);
+        assertThrows(NotFoundException.class, () -> {lobbyService.addUserToExistingLobby(testUser2, createdLobby);});
+
+
+
 
     }
 
@@ -174,7 +208,9 @@ public class LobbyServiceTest {
         Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser);
         userService.createUser(testUser);
         testlobby.addUser(testUser.getId());
+
         // Mockito.when(lobbyRepository.findById(Mockito.any())).thenReturn(Optional.ofNullable(testlobby));
+
         Mockito.when(userService.getUserByUserId(Mockito.anyLong())).thenReturn(testUser);
         // Lobby createdLobby = lobbyService.createLobby(testlobby);
         lobbyService.deleteLobby(testlobby.getId());
@@ -186,6 +222,63 @@ public class LobbyServiceTest {
         assertEquals(false, testUser.getInLobby());
 
     }
+
+
+    @Test
+    public void userExitLobbyTest() {
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser);
+        userService.createUser(testUser);
+        testlobby.addUser(testUser.getId());
+
+        //change setup so user is in lobby
+        testUser.setInLobby(true);
+
+        Mockito.when(userService.getUserByUserId(Mockito.anyLong())).thenReturn(testUser);
+
+        lobbyService.addUserToExistingLobby(testUser2, testlobby);
+        lobbyService.userExitLobby(testUser,testlobby.getId());
+        // then
+        Mockito.verify(lobbyRepository, Mockito.times(1)).flush();
+
+
+        assertFalse( testUser.getInLobby());
+        assertTrue(testUser2.getInLobby());
+
+    }
+
+    @Test
+    public void userExitLobbyTestFailUserNotInLobby() {
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser);
+        userService.createUser(testUser);
+        testlobby.addUser(testUser.getId());
+
+        Mockito.when(userService.getUserByUserId(Mockito.anyLong())).thenReturn(testUser);
+        // Lobby createdLobby = lobbyService.createLobby(testlobby);
+        lobbyService.addUserToExistingLobby(testUser2, testlobby);
+        assertThrows(PreconditionFailedException.class, () -> {lobbyService.userExitLobby(testUser,testlobby.getId());});
+
+
+    }
+
+
+    @Test
+    public void userExitLobbyTestFailUserNotInThisLobby() {
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(testUser);
+        userService.createUser(testUser);
+        testlobby.addUser(testUser.getId());
+
+        //set user in lobby to true, "Mock that he is a lobby"
+        testUser.setInLobby(true);
+
+        Mockito.when(userService.getUserByUserId(Mockito.anyLong())).thenReturn(testUser);
+
+
+        assertThrows(PreconditionFailedException.class, () -> {lobbyService.userExitLobby(testUser2,testlobby.getId());});
+
+
+    }
+    
+
 }
 
 
